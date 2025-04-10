fcdef2bf7f19b670e6ce5bbbc61aff5fe068a5b5
Guillaume SOUEDE <guillaume.souede@univ-tlse3.fr>
2025-04-10 13:49:07 +0200

    Fix try on Main

--- Motus 2/src/Main.java
+++ Motus 2/src/Main.java
@@ -2,36 +2,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        OuvrirDB db = new OuvrirDB(); // ici on a l'ouverture + création de la DB
+        try (Scanner scanner = new Scanner(System.in)) {
+            OuvrirDB db = new OuvrirDB(); // ici on a l'ouverture + création de la DB
 
-        do {
-            System.out.println("Bienvenue dans ⓂⓄⓉⓊⓈ !");
-            System.out.println("Choisissez un mode : 1️⃣(🫵 vous devinez) ou 2️⃣ (🤖 l'ordinateur devine)");
-            int mode = scanner.nextInt();
-            scanner.nextLine();
+            do {
+                System.out.println("Bienvenue dans ⓂⓄⓉⓊⓈ !");
+                System.out.println("Choisissez un mode : 1️⃣(🫵 vous devinez) ou 2️⃣ (🤖 l'ordinateur devine)");
+                int mode = scanner.nextInt();
+                scanner.nextLine();
 
-            if (mode == 1) {
-                JeuJoueur.joueurDevine(scanner, db);
-            } else if (mode == 2) {
-                String motSecret;
-                do {
-                    System.out.println("Entrez un mot secret pour l'ordinateur (6, 7, 8 ou 9 lettres) : ");
-                    motSecret = scanner.nextLine();
-                    if (motSecret.length() < 6 || motSecret.length() > 9) {
-                        System.out.println("❌ Mot invalide. Le mot doit contenir 6, 7, 8 ou 9 lettres.");
-                    }
-                } while (motSecret.length() < 6 || motSecret.length() > 9);
+            switch (mode) {
+                case 1 -> JeuJoueur.joueurDevine(scanner, db);
+                case 2 -> {
+                    String motSecret;
+                    do {
+                        System.out.println("Entrez un mot secret pour l'ordinateur (6, 7, 8 ou 9 lettres) : ");
+                        motSecret = scanner.nextLine();
+                        if (motSecret.length() < 6 || motSecret.length() > 9) {
+                            System.out.println("❌ Mot invalide. Le mot doit contenir 6, 7, 8 ou 9 lettres.");
+                        }
+                    } while (motSecret.length() < 6 || motSecret.length() > 9);
 
-                JeuOrdinateur.ordinateurDevine(motSecret, scanner, db);
-            } else {
-                System.out.println("Mode invalide.");
+                    JeuOrdinateur.ordinateurDevine(motSecret, scanner, db);
+                }
+                default -> System.out.println("Mode invalide.");
             }
 
             System.out.println("⏯️ Jouer à nouveau ? 1️⃣ : oui, 2️⃣ : non");
         } while (scanner.nextLine().equalsIgnoreCase("1"));
 
-        scanner.close();
-        System.out.println("👋 Au revoir.");
+            System.out.println("👋 Au revoir.");
+        }
     }
 }
