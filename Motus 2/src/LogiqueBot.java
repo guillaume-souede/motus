6ed491bc2a247f77b507d37c2e019a069d9c9b1d
Guillaume SOUEDE <guillaume.souede@univ-tlse3.fr>
2025-04-10 14:21:51 +0200

    Update LogiqueBot.java

--- Motus 2/src/LogiqueBot.java
+++ Motus 2/src/LogiqueBot.java
@@ -2,41 +2,47 @@ import java.util.ArrayList;
 
 public class LogiqueBot {
 
-    
     public ArrayList<String> choix(String progVraie, String charsMalPlace, OuvrirDB dicoMots){
-        int taille =  progVraie.length(); // extraction de la taille
+        // Extraction de la taille du Mot
+        int taille =  progVraie.length();
+        // Extraction des mots de taille égale au mot cherché
+        Mots phrase = dicoMots.getPhrase().get(taille);
+        // Passage au format Liste dynamique
+        ArrayList<String> sousList = phrase.getLine();
 
-        Mots phrase = dicoMots.getPhrase().get(taille); // extraction des mots de taille équivalente au mot cherché
-        
-        ArrayList<String> sousList = phrase.getLine(); // mise sous forme de Liste dynamique
-
-
+        // FILTRAGE DES MOTS IMPOSSIBLES EN 2 ETAPES
+        // étape 1 : filtrer les mots qui n'ont pas le bon caractère à la bonne position
         for (int i = 0; i < taille; i++) {
-            char charAt = progVraie.charAt(i); // stockage du char a la position actuelle
-
+            // Stockage du caractère (!attention!) à la position actuelle
+            char charAt = progVraie.charAt(i);
+            // Test si le caractère est une lettre ou un astérisque (*)
             if (charAt != '*'){
+                // Retrait des mots qui n'ont pas le caractère à la bonne position
                 for (String mot : sousList) {
-                    if (mot.charAt(i) != charAt) {sousList.remove(mot);} // on retire les mots n'ayant pas le char a la bonne position
+                    if (mot.charAt(i) != charAt) {sousList.remove(mot);}
                 }
             }
         }
-        // étape 2 éliminer tous ceux qui n'ont pas 
 
+        // étape 2 : filtrer les mots qui n'ont pas le bon caractère à la mauvaise position
         if (sousList != null) {
             for (String mot : sousList) {
                 if (testCharMalPlace(mot, charsMalPlace) != true){
-                    sousList.remove(mot); // si un des mots n'a pas  
+                    sousList.remove(mot);  
                 }
             }
         }
         // ya eu filtrage des mots impossibles, retour a vous la télé
+        // Ici la télé, merci Jan.
             return sousList;
         }
 
+    // Test de la présence de caractères mal placés
     private Boolean testCharMalPlace (String mot, String lettres){
         for (int i = 0; i < lettres.length(); i++) {
             String charActu = lettres.substring(i, i+1);
-            if (mot.contains(charActu) == false) { // test si le mot contient les lettres
+            // test si le mot contient bien le caractère
+            if (mot.contains(charActu) == false) {
                     return false;
                 }
             
@@ -45,4 +51,3 @@ public class LogiqueBot {
     }
 
 }
-
