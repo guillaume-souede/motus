7ef6c91437a264d4736139c691e4f8aaaa909ccf
Jan <janamouroux@gmail.com>
2025-04-10 15:52:23 +0200

    foo

--- Motus 2/src/LogiqueBot.java
+++ Motus 2/src/LogiqueBot.java
@@ -2,43 +2,43 @@ import java.util.ArrayList;
 
 public class LogiqueBot {
 
-    public ArrayList<String> choix(String progVraie, String charsMalPlace, OuvrirDB dicoMots){
-        // Extraction de la taille du Mot
-        int taille =  progVraie.length();
-        // Extraction des mots de taille égale au mot cherché
-        Mots phrase = dicoMots.getPhrase().get(taille);
+    static public ArrayList<String> choix(String progVraie, String charsMalPlace, ArrayList<String> dicoMots){
+        /* si on injecte directement des objets OuvrirDb (pas bien) */
+        // // Extraction de la taille du Mot
+        // int taille =  progVraie.length();
+        // // Extraction des mots de taille égale au mot cherché
+        // Mots phrase = dicoMots.getPhrase().get(taille);
         // Passage au format Liste dynamique
-        ArrayList<String> sousList = phrase.getLine();
 
-        // FILTRAGE DES MOTS IMPOSSIBLES EN 2 ETAPES
+        // FILTRAGE DES MOTS IMPOSSIBLES EN 2 ETAPES : 
         // étape 1 : filtrer les mots qui n'ont pas le bon caractère à la bonne position
-        for (int i = 0; i < taille; i++) {
+        for (int i = 0; i < dicoMots.size(); i++) {
             // Stockage du caractère (!attention!) à la position actuelle
             char charAt = progVraie.charAt(i);
             // Test si le caractère est une lettre ou un astérisque (*)
             if (charAt != '*'){
                 // Retrait des mots qui n'ont pas le caractère à la bonne position
-                for (String mot : sousList) {
-                    if (mot.charAt(i) != charAt) {sousList.remove(mot);}
+                for (String mot : dicoMots) {
+                    if (mot.charAt(i) != charAt) {dicoMots.remove(mot);}
                 }
             }
         }
 
         // étape 2 : filtrer les mots qui n'ont pas le bon caractère à la mauvaise position
-        if (sousList != null) {
-            for (String mot : sousList) {
+        if (dicoMots != null) {
+            for (String mot : dicoMots) {
                 if (testCharMalPlace(mot, charsMalPlace) != true){
-                    sousList.remove(mot);  
+                    dicoMots.remove(mot);  
                 }
             }
         }
         // ya eu filtrage des mots impossibles, retour a vous la télé
         // Ici la télé, merci Jan.
-            return sousList;
+            return dicoMots;
         }
 
     // Test de la présence de caractères mal placés
-    private Boolean testCharMalPlace (String mot, String lettres){
+    private static Boolean testCharMalPlace (String mot, String lettres){
         for (int i = 0; i < lettres.length(); i++) {
             String charActu = lettres.substring(i, i+1);
             // test si le mot contient bien le caractère
