5a5162e9683dd45888f42116f6c7841dc19c9ae5
Jan <janamouroux@gmail.com>
2025-04-10 08:41:40 +0200

    UPDATE_filereader

--- Motus 2/src/OuvrirDB.java
+++ Motus 2/src/OuvrirDB.java
@@ -30,24 +30,7 @@ public class OuvrirDB {
                 if (contentLine != null)  {
                     int fooKey = contentLine.length();
                     if (phrase.get(fooKey) == null) {
-                        contentLine = contentLine.replace(",", "")
-                        .replace("œ", "oe")
-                        .replace("æ", "ae")
-                        .replace("é", "e")
-                        .replace("è", "e")
-                        .replace("ê", "e")
-                        .replace("ë", "e")
-                        .replace("à", "a")
-                        .replace("â", "a")
-                        .replace("ä", "a")
-                        .replace("ç", "c")
-                        .replace("î", "i")
-                        .replace("ï", "i")
-                        .replace("ô", "o")
-                        .replace("ö", "o")
-                        .replace("ù", "u")
-                        .replace("û", "u")
-                        .replace("ü", "u");
+                        
                         phrase.put(fooKey, new Mots(contentLine));
                     }
                     else {
@@ -67,8 +50,7 @@ public class OuvrirDB {
     public void initialisationDB(){
         ArrayList<String> cheminDB = new ArrayList<>();
         // ajout des fichier
-        cheminDB.add("data/mots_6_lettres.csv");    cheminDB.add("data/mots_7_lettres.csv");    
-        cheminDB.add("data/mots_8_lettres.csv");    cheminDB.add("data/mots_9_lettres.csv"); 
+        cheminDB.add("data/motsMotus.txt");
 
         // ouverture et stockege des donénes
         for (String string : cheminDB) {
