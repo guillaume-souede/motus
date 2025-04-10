aa65706dcbc1359f7888d9b57eba169af3259e96
Guillaume SOUEDE <guillaume.souede@univ-tlse3.fr>
2025-04-10 14:25:47 +0200

    Update Mots.java

--- Motus 2/src/Mots.java
+++ Motus 2/src/Mots.java
@@ -5,20 +5,22 @@ public class Mots {
     private int nChar;
     ArrayList<String> line;
 
-
+    // Retourner la ligne
     public ArrayList<String> getLine() {
         return line;
     }
-
+    // Retourner la taille du mot occupant la ligne
     public int getnChar() {
         return nChar;
     }
 
+    // Liste de Mots
     public Mots(String line){
         this.line = new ArrayList<>();
         this.line.add(line);
     }
 
+    // Agrandir la liste de Mots
     public void extend(String mot){
         line.add(mot);
     }
