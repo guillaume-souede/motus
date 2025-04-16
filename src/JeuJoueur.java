7d736550ad42805237a95d4a8e2c2bc062bd496e
Jan <janamouroux@gmail.com>
2025-04-16 11:09:02 +0200

    UPDATE_JeuJoueur_for_new_EtatMots

--- src/JeuJoueur.java
+++ src/JeuJoueur.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class JeuJoueur {
-    private static final int essaisMax = 6;
+    private static final int essaisMax = 9;
     private static final String GRAS = "\033[1m";
     private static final String RESET = "\033[0m";
 
@@ -11,7 +11,7 @@ public class JeuJoueur {
         int tailleMots = random.nextInt(3)+6;   // get random length
         listMot = db.getAllPhrase().get(tailleMots).line;  
         String motSecret = listMot.get(random.nextInt(listMot.size())); // get random mot
-        MotATrouver motATrouver = new MotATrouver(motSecret);
+        // MotATrouver motATrouver = new MotATrouver(motSecret);
             // la on a ouvert la db, et choisis un mot au hazard
         System.out.println("🫵 Vous devez deviner un mot de " + GRAS + motSecret.length() + " lettres en " + essaisMax + " tentatives" + RESET + ".");
 
@@ -25,9 +25,9 @@ public class JeuJoueur {
                 continue;
             }
 
-            motATrouver.afficherEtatMot(proposition);
+            EtatMot.pprint(proposition, motSecret);
 
-            if (motATrouver.verifierProposition(proposition)) {
+            if (EtatMot.checkEtatMot(proposition, motSecret).equals(motSecret)) {
                 System.out.println("🥳 Félicitations !" + GRAS + "ⓂⓄⓉⓊⓈ en " + essai + " essai(s) sur " + essaisMax + RESET + ".");
                 return;
             }
@@ -42,4 +42,11 @@ public class JeuJoueur {
         return new String[]{"avion", "voiture", "maison", "ordinateur", "bureau", "table", "chaise", "fenetre"};
     }
     */
+
+    // un run de test
+    public static void main(String[] args) {
+        OuvrirDB db = new OuvrirDB(); // ouvrir les mots
+        Scanner scanner = new Scanner(System.in); // le scanner pour l'input (pour tester)
+        joueurDevine(scanner, db);
+    }
 }
